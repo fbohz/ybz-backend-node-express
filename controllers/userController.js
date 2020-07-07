def32196114c1a6bfe2319ed45de31cea716f5b8
Felipe <fb.dc.col@gmail.com>
2020-07-06 19:58:17 -0500

    user controller work

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+const User = require('../models/userModel')
+
+exports.getAllUsers = async (req, res) => {
+  
+    try {
+      const users = await User.find();
+      res.status(200).json({
+        status: 'success',
+        results: users.length,
+        users,
+      });
+    } catch (error) {
+      res.status(404).json({
+        status: 'fail',
+        message: error,
+      });
+    }
+  };
+  
